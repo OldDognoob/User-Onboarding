77e2a43d175150703cf7fea6cd17b8e02f268612
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2019-11-14 18:33:08 +0200

    initial changes

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -2,11 +2,22 @@ import React from 'react';
 
 import './App.css';
 import FormikForm from './Form';
+import Axios from 'axios';
 
 function App() {
+  const submitForm = () => {
+    console.log()
+
+    axios.post('')
+    .then((response) => {
+      console.log(response)
+    })
+  }
   return (
     <div className="App">
       <FormikForm/>
+      <FormkForm submit ={submitForm}>
+      </FormkForm>
     </div>
   );
 }
